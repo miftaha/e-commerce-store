1302767090be2689624dd33cb8854de6ed8def67
Miftah <miftahaadem@gmail.com>
2025-01-31 10:09:26 +0300

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { fetchProducts } from '@/lib/api'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 const Home = () => {
   const [products, setProducts] = useState([])
@@ -8,15 +9,19 @@ const Home = () => {
   return (
     <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {products.map((product: any) => (
-        <div key={product.id} className="border p-4 rounded-lg">
+        <Link
+          href={`/product/${product.id}`}
+          key={product.id}
+          className="border cursor-pointer rounded-lg p-4 hover:shadow-lg"
+        >
           <img
             src={product.image}
             alt={product.title}
-            className="w-full h-48 object-cover"
+            className="w-full h-48 object-cover rounded-md"
           />
           <h2 className="font-bold text-lg">{product.title}</h2>
           <p className="text-gray-500">${product.price}</p>
-        </div>
+        </Link>
       ))}
     </div>
   )
